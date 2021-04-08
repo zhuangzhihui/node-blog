f0171ef1b7e7b27271831f92d58a09fa7b62bd53
黄志辉 <1367428189@qq.com>
2021-04-08 20:39:03 +0800

    路由开发-博客列表路由

--- blog1/app.js
+++ blog1/app.js
@@ -1,12 +1,17 @@
+const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
 const serverHandle = (req, res) => {
-    //设置返回格式 JSON
+    // 设置返回格式 JSON
     res.setHeader('Content-type', 'application/json')
 
+    // 获取path
     const url = req.url
     req.path = url.split('?')[0]
+
+    // 解析query
+    req.query = querystring.parse(url.split('?')[0])
 
     // 处理blog路由
     const blogData = handleBlogRouter(req, res)
---
+++ blog1/src/controller/blog.js
@@ -0,0 +1,23 @@
+const getList = (author, keyword) => {
+    // 先返回假数据（格式是正确的）
+    return [
+        {
+            id: 1,
+            title: '标题A',
+            content: '内容A',
+            createTime: 1617884333034,
+            author: '张三'
+        },
+        {
+            id: 2,
+            title: '标题B',
+            content: '内容B',
+            createTime: 1617884333044,
+            author: '李四'
+        }
+    ]
+}
+
+module.exports = {
+    getList
+}
